9f6a8ae5501bea780b8cd504925cc25642985913
Anggasayogo <anggasayogosm@gmail.com>
2020-04-13 01:22:13 +0700

    persiapan form data

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, Image} from 'react-native';
-import {Input} from '../../components';
+import {Input, Button} from '../../components';
 import {RegisterImage} from '../../assets';
 import {colors} from '../../utils';
 import {BackIcon} from '../../assets';
+import {ScrollView} from 'react-native-gesture-handler';
 
 const Register = () => {
+  const [form, setForm] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+  });
+
+  const sendData = () => {
+    console.log('data yang dikirim', form);
+  };
+
+  const onInputChange = (value, input) => {
+    setForm({
+      ...form,
+      [input]: value,
+    });
+  };
   return (
     <View style={styles.wrapper.page}>
-      <Image source={BackIcon} style={styles.wrapper.iconBack} />
-      <Image source={RegisterImage} style={styles.wrapper.imageHero} />
-      <Text style={styles.wrapper.title}>
-        Mohon mengisi form register untuk pembuatan account anda
-      </Text>
-      <View style={styles.space(64)} />
-      <Input placeholder="Nama Lengkap" />
-      <View style={styles.space(33)} />
-      <Input placeholder="Email" />
-      <View style={styles.space(33)} />
-      <Input placeholder="Password" />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Image source={BackIcon} style={styles.wrapper.iconBack} />
+        <Image source={RegisterImage} style={styles.wrapper.imageHero} />
+        <Text style={styles.wrapper.title}>
+          Mohon mengisi form register untuk pembuatan account anda
+        </Text>
+        <View style={styles.space(64)} />
+        <Input
+          placeholder="Nama Lengkap"
+          value={form.fullName}
+          onChangeText={value => onInputChange(value, 'fullname')}
+        />
+        <View style={styles.space(33)} />
+        <Input
+          placeholder="Email"
+          value={form.email}
+          onChangeText={value => onInputChange(value, 'email')}
+        />
+        <View style={styles.space(33)} />
+        <Input
+          placeholder="Password"
+          value={form.password}
+          onChangeText={value => onInputChange(value, 'password')}
+          secureTextEntry={true}
+        />
+        <View style={styles.space(83)} />
+        <Button title="Register" onPress={sendData} />
+      </ScrollView>
     </View>
   );
 };
